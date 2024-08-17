fe3ea14dd6cdc8129e2891e8b28af2ad20e0e46d
Joey Sardella <josephsardella@gmail.com>
2024-08-17 16:50:12 -0700

    added mobile hero section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,46 +1,62 @@
 /* eslint-disable react/no-unknown-property */
-import { Suspense, useEffect, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
-import CanvasLoader from '../Loader';
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+const Computers = ({isMobile}) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={1.5}
-      groundColor='black' />
+      <hemisphereLight intensity={1.5} groundColor="black" />
       <pointLight intensity={1.5} />
-      <primitive 
-      object={computer.scene}
-      scale={0.65}
-      position={[0, -2, -1.5]}
-      rotation={[-0.01, -0.2, -0.1]}
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.5 : 0.65}
+        position={isMobile ? [0, -1.25, -1.5] : [0, -2, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
-    frameLoop="demand"
-    shadows
-    camera={{ position: [20, 3, 5], fov: 25}}
-    gl={{ preserveDrawingBuffer: true }}
+      frameLoop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-      <OrbitControls 
-      enableZoom={false} 
-      maxPolarAngle={Math.PI / 2 }
-      minPolarAngle={Math.PI / 2 }
-      />
-      <Computers />
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
 export default ComputersCanvas;
