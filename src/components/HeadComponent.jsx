b91773eb8d7e9631d98b19c1d092b15bcb0d1994
Joey Sardella <josephsardella@gmail.com>
2024-08-20 14:14:11 -0700

    added twitter tags & adjusted thumbnail image

--- src/components/HeadComponent.jsx
+++ src/components/HeadComponent.jsx
@@ -8,6 +8,11 @@ const HeadComponent = () => (
         <meta property="og:image" content={thumbnail} />
         <meta property="og:url" content="https://dreambigjoe.com" />
         <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Think Big Joe | Web Developer" />
+        <meta name="twitter:description" content="Joseph Sardella is a Full-Stack Web Developer with experience in Reactjs, Nodejs, Threejs, MongoDB, Nextjs & Gatsby. He also is a Generative AI artist, Video & SFX Editor & Graphic Designer. Contact today to elevate your business and Dream Big with Joe!" />
+        <meta name="twitter:image" content={thumbnail} />
     </Helmet>
 );
 
