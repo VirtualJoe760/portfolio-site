beb315833db143085beee389e1f2ba1ec15c0ce1
Joey Sardella <josephsardella@gmail.com>
2024-08-20 14:38:47 -0700

    changed thumbnail link

--- src/components/HeadComponent.jsx
+++ src/components/HeadComponent.jsx
@@ -1,18 +1,18 @@
 import { Helmet } from 'react-helmet';
-import { thumbnail } from '../assets';
+
 
 const HeadComponent = () => (
     <Helmet>
-        <meta property="og:title" content="Think Big Joe | Web Developer" />
+        <meta property="og:title" content="Dream Big Joe | Web Developer" />
         <meta property="og:description" content="Joseph Sardella is a Full-Stack Web Developer with experience in Reactjs, Nodejs, Threejs, MongoDB, Nextjs & Gatsby. He also is a Generative AI artist, Video & SFX Editor & Graphic Designer. Contact today to elevate your busines and Dream Big with Joe!" />
-        <meta property="og:image" content={thumbnail} />
+        <meta property="og:image" content="/src/assets/ai-thumbnail.png" />
         <meta property="og:url" content="https://dreambigjoe.com" />
         <meta property="og:type" content="website" />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Think Big Joe | Web Developer" />
+        <meta name="twitter:title" content="Dream Big Joe | Web Developer" />
         <meta name="twitter:description" content="Joseph Sardella is a Full-Stack Web Developer with experience in Reactjs, Nodejs, Threejs, MongoDB, Nextjs & Gatsby. He also is a Generative AI artist, Video & SFX Editor & Graphic Designer. Contact today to elevate your business and Dream Big with Joe!" />
-        <meta name="twitter:image" content={thumbnail} />
+        <meta name="twitter:image" content="/src/assets/ai-thumbnail.png" />
     </Helmet>
 );
 
