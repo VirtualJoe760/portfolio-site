f0d3fd75d73bb0ffb21e4701a80869fc7160ee80
Joey Sardella <josephsardella@gmail.com>
2024-08-20 15:00:23 -0700

    added meta's to index.html

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HeadComponent } from '/src/components'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HeadComponent />
+    
     <App />
   </StrictMode>,
 )
